fe10381fa346438ca2350952b8102c5100f1c9a2
sarahvic <sarahley93@gmail.com>
2020-02-25 15:02:08 +0100

    asdfasd

--- carRental/src/app/app.module.ts
+++ carRental/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- carRental/src/app/contact/contact.component.ts
+++ carRental/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  info = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [Validators.required, Validators.email]),
+  message : new FormControl('',Validators.required),
+});
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(){
+    if(this.info.valid){
+      var a = this.info.value;
+      console.log(a)
+    }
+    }
 
 }
